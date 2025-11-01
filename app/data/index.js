54facbbb9c0a4d91d70af291b48c93d362c15e5f
Youcef Taleb <youcefusa2@gmail.com>
2025-11-01 14:35:21 +0100

    I created the layout for the categories page and added a data directory and some svg illustrations for categories

---
+++ app/data/index.js
@@ -0,0 +1,14 @@
+
+// static data to use for now
+export const categories = [
+    { name: "Pentesting", img: "/illustrations/Cloud hosting-amico.svg", ntools: 245 },
+    { name: "OSINT", img: "/illustrations/Endpoint-bro.svg", ntools: 189 },
+    { name: "Reverse engineering", img: "/illustrations/Operating system-bro.svg", ntools: 152 },
+    { name: "Forensics", img: "/illustrations/Endpoint-rafiki.svg", ntools: 128 },
+    { name: "Exploitation", img: "/illustrations/Cyber attack-bro.svg", ntools: 98 },
+    { name: "Malware analysis", img: "/illustrations/Cyber attack-pana.svg", ntools: 76 },
+    { name: "Networking", img: "/illustrations/Online world-cuate.svg", ntools: 210 },
+    { name: "Cryptography", img: "/illustrations/Login-rafiki.svg", ntools: 85 },
+    { name: "Cloud security", img: "/illustrations/Server-rafiki.svg", ntools: 64 },
+    { name: "Web security", img: "/illustrations/Security On-amico.svg", ntools: 199 },
+];
